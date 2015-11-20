cd8edbfda6b7bb6b7c60a1ba1396b3626bb9d23d
Yilen <yilen.pan@gmail.com>
2015-11-19 21:40:00 -0800

    auth complete

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,15 +1,14 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-// var passport = require('passport');
 var session = require('express-session');
-// var facebookStrategy = require('../services/passportStrategies');
+var passport = require('./passport');
 var path = require('path');
 
 module.exports = function (app, express) {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static(path.join(__dirname, '/../client')));
+  app.use(express.static(path.join(__dirname, '/../../client')));
 
   // Uses sessions
   app.use(session({
@@ -18,10 +17,8 @@ module.exports = function (app, express) {
     saveUninitialized: false
   }));
 
-  // facebookStrategy(passport);
-
-  // app.use(passport.initialize());
-  // app.use(passport.session());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   //Set up routers
   var authRoute = express.Router();
