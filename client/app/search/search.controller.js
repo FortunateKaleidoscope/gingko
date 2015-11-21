03bac13ec3c17b2b15d12dc130e2241881cec5d3
Yilen <yilen.pan@gmail.com>
2015-11-20 16:18:40 -0800

    changed controller

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -15,7 +15,7 @@
     self.isDisabled = false;
     // below is a hack for testing, we are struggling to access facebook auth username from client side
     self.meal = {
-      username: 'BALLS'
+      username: 'Cory'
     };
 
     self.attendees = [1,2,3,4,5,6,7,8,9];
