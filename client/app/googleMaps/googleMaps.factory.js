01c749e0e2cc563184f38e0e383859b2717cfbcb
Yilen <yilen.pan@gmail.com>
2015-11-22 11:45:34 -0800

    added comments

--- client/app/googleMaps/googleMaps.factory.js
+++ client/app/googleMaps/googleMaps.factory.js
@@ -10,25 +10,28 @@
     var services = {
       getLocation : getLocation,
       initMap: initMap,
-
       clearMarkers: clearMarkers,
       showMarkers: showMarkers,
       makeMarker: makeMarker
     };
     return services;
 
+    // To clear the markers, pass each marker a null map
     function clearMarkers (markers) {
       for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
       }
     }
-
+    // To show markers, pass each created marker the created map
     function showMarkers (markers, map) {
       for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(map);
       }
     }
 
+    // To make a marker, pass makeMarker the map, lat/lng, and info for the mouseover window
+    // it will return a marker object which you can then push into an array
+    // then pass it showMarkers to show the markers
     function makeMarker (map, lat, lng, info) {
       var myLatLng = new google.maps.LatLng(lat, lng);
       var infowindow = new google.maps.InfoWindow({
@@ -47,17 +50,22 @@
       return marker;
     };
 
+    // Turns an address into a lat/lng object
     function getLocation (address, cb) {
       var geocoder = new google.maps.Geocoder();
       geocoder.geocode({
         address: address
-      }, function (result, status) {
-        cb(result, status);
+      }, function (results, status) {
+        // results is an array of objects
+          // results[0].geometry.location.lat() i think
+        // TODO: Error handle with status
+        cb(results, status);
       });
     }
-
+    // pass initMap the domNode you want to turn into a map
+    // along with the starting lat/lng
     function initMap (domNode, lat, lng) {
-      var latlng = new google.maps.LatLng( lat, lng );//sets to San Francisco by default
+      var latlng = new google.maps.LatLng( lat, lng );
       var mapOptions = {
         zoom: 12,
         center: latlng
--- client/app/meals/meals.controller.js
+++ client/app/meals/meals.controller.js
@@ -23,31 +23,40 @@
 
 
     self.getMeals = function () {
+      // TODO: instead of getMeals, calling it getMealsBySearchTerm
       MealsFactory.getMeals()
       .then(function (data) {
+        // Filter out bad data
         self.meals = _.filter(data, function (meal) {
           return meal.restaurant !== null;
         }).map(function (meal) {
+          // TODO: Dummy Images
           meal.meal.img = images[Math.floor(Math.random() * images.length)];
           return meal;
         });
 
+        // pass getLocation the currentCity and a callback
         getLocation($state.params.searchTerm, function (result, status) {
+          // create a map using initMap and set it to $scope.map
           self.map = initMap(document.getElementById('map'),
+            // pass lat/lng to initMap
             result[0].geometry.location.lat(),
             result[0].geometry.location.lng());
 
+          // Create markers by mapping over the meals and passing
+          // makeMarker the map, lat/lng, and the meal title
           self.markers = self.meals.map(function (meal) {
             return makeMarker(self.map,
               meal.restaurant.lat,
               meal.restaurant.lng,
               meal.meal.title);
           });
-
+          // show the markers
           showMarkers(self.markers, self.map);
         });
       });
     };
+    
     self.getMeals();
   }
 })();
